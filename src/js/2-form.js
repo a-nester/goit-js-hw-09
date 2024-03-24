3589c07721e0fa644970924f9653b7634a93d607
a-nester <a_nester@ukr.net>
2024-03-24 19:37:59 +0200

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,25 @@
 const form = document.querySelector(".feedback-form");
+const input = form.elements[0];
+const message = form.elements[1];
 
-console.log(form);
+const localStorageKey = "feedback-form-state";
+const container = JSON.parse(localStorage.getItem(localStorageKey)) ?? {};
+
+input.value = container.input ?? "";
+message.value = container.message ?? "";
+
+form.addEventListener("input", (event) => {
+        const input = event.currentTarget[0].value;
+        const message = event.currentTarget[1].value;
+        container.input = input;
+        container.message = message;
+        localStorage.setItem(localStorageKey, JSON.stringify(container));
+});
+    // Remove data from storage after submit
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(container);
+    input.value = "";
+    message.value = "";
+    localStorage.removeItem(localStorageKey);
+})
